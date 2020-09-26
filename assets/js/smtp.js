d8a8c5e3c12a95f543dadd73ed8c0523aa2e4511
Sanjeev Chauhan <isitsanjeev@gmail.com>
2020-09-26 15:14:20 +0530

    Update Data

--- assets/js/smtp.js
+++ assets/js/smtp.js
@@ -3,7 +3,7 @@ function sendEmail() {
             Host: "smtp.mailtrap.io",
             Username: "<Mailtrap username>",
             Password: "<Mailtrap password>",
-            To: 'recipient@example.com',
+            To: 'isitsanjeev@gmail.com',
             From: "sender@example.com",
             Subject: "Test email",
             Body: "<html><h2>Header</h2><strong>Bold text</strong><br></br><em>Italic</em></html>"
